b22f9fff5611f8cf86a9fbc09042587b9348fabe
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-12-02 08:40:22 -0500

    fix: the file vite.config.js was created

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+export default {
+    base: '/Pascua/',  // Ajusta el nombre del repositorio aquí (nombre del repositorio en GitHub)
+    build: {
+      outDir: 'dist',  // Directorio de salida donde se generarán los archivos estáticos
+    },
+  }
